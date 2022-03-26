4e2189553b8a6e4b05bb489f391deec65bc909ea
NitishGreens <nitishgreens@gmail.com>
2022-03-26 13:53:25 +0530

    task done

---
+++ src/com/hexa/Employee.java
@@ -0,0 +1,14 @@
+package com.hexa;
+
+public class Employee {
+
+	public void add() {
+		System.out.println("add");
+	}
+
+	public static void main(String[] args) {
+		Employee e = new Employee();
+		e.add();
+	}
+
+}
